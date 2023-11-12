29fc2b649776a0c884fc93df5af40b27bca98d06
TonyBarretor <tony.barretor@gmail.com>
2023-11-12 15:39:10 -0800

    feat: adding task 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.8
+import dis
+import hidden_4
+
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
+    names.sort()
+
+    for name in names:
+        print(name)
