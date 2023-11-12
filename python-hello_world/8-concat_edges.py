df27dea2546d17237a1bc18ac48ebcede98d915a
TonyBarretor <tony.barretor@gmail.com>
2023-11-11 19:47:07 -0800

    fix: concatenate using join for list, and split for specific words. Remember also split returns list if you select a range

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-print(' '.join(str.split()[5:7]) + ' ' + str.split()[-4] + ' ' + str.split()[0])
+str = f"{' '.join(str.split()[5:7])} {str.split()[-4]} {str.split()[0]}"
+print(str)
