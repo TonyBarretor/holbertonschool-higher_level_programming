14626d4e6daa11f48d108c248c79c78dc53cbbca
TonyBarretor <tony.barretor@gmail.com>
2023-11-12 14:31:31 -0800

    feat: add task 0

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+
+local_namespace = {}
+exec(open("calculator_1.py").read(), local_namespace, local_namespace)
+
+add = local_namespace["add"]
+sub = local_namespace["sub"]
+mul = local_namespace["mul"]
+div = local_namespace["div"]
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("{} + {} = {}".format(a, b, result_add))
+print("{} - {} = {}".format(a, b, result_sub))
+print("{} * {} = {}".format(a, b, result_mul))
+print("{} / {} = {}".format(a, b, result_div))
