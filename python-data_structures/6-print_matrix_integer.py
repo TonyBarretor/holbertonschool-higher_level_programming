4d12c2e324cf41c0568d7b3ce5013b6519dfe63e
TonyBarretor <tony.barretor@gmail.com>
2023-11-12 19:05:22 -0800

    fix: new code to 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = ''.join(char for char in my_string if char.lower() != 'c')
-
-    return new_string
+        return ''.join([char for char in my_string if char.lower() != 'c'])
