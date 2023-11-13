8f557f7c95c15f9b92ab1b62da7d8cffb6332741
TonyBarretor <tony.barretor@gmail.com>
2023-11-12 20:17:01 -0800

    fix: 2 and 3

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-        print(chr(i), end='')
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print("{}".format(chr(char)), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print("{}".format(chr(char)), end="")
