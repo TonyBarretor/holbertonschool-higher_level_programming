ddafa692e1601da5f89422be4a0d814f0b6738c4
TonyBarretor <tony.barretor@gmail.com>
2023-11-19 19:39:06 -0800

    fix:add documentation task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Empty class Square that defines a square.
+
+    Attributes:
+        No attributes are defined explicitly in the class.
+        The class serves as a placeholder for a square.
+
+    Methods:
+        No methods are defined explicitly in the class.
+
+    Usage:
+        square_instance = Square()
+        # Create an instance of the Square class.
+
+        print(square_instance.__dict__)
+        # Access the dictionary containing the object's attributes.
+    """
     pass
