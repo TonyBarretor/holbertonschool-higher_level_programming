a34c5b5caec0edb99128bb7e6cdea63ef6c1583a
TonyBarretor <tony.barretor@gmail.com>
2023-11-21 14:32:21 -0800

    fix: doc charaters

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,43 +1,27 @@
 #!/usr/bin/python3
 """
 0-square module
-
 ====================
-
 This module defines the Square class.
-
 Classes:
     Square: An empty class representing a square.
-
-Attributes:
-    None.
-
-Methods:
-    None.
-
+Attributes: None.
+Methods: None.
 Usage:
     from 0-square import Square
-
     square_instance = Square()
     # Create an instance of the Square class.
-
     print(square_instance.__dict__)
     # Access the dictionary containing the object's attributes.
 """
 
 
 class Square:
+
     """
     A class representing a square.
-
     This class is an empty placeholder for future implementations of a square.
-    It provides a foundation for potential attributes and methods related to squares.
-
-    Attributes:
-        None.
-
-    Methods:
-        None.
+    Attributes: None.
+    Methods: None.
     """
-
     pass
