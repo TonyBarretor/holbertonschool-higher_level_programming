b19894825c8558b70f9689691fd632e5f9d2a676
TonyBarretor <tony.barretor@gmail.com>
2023-11-26 15:09:59 -0800

    fix: add documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
 
+"""
+0-rectangle module
+====================
+This module defines the Square class.
+Classes:
+    Square: An empty class representing a square.
+Attributes: None.
+Methods: None.
+Usage:
+    from 0-square import Square
+    square_instance = Square()
+    # Create an instance of the Square class.
+    print(square_instance.__dict__)
+    # Access the dictionary containing the object's attributes.
+"""
+
+
 class Rectangle:
+
+    """
+    A class representing a square.
+    This class is an empty placeholder for future implementations of a square.
+    Attributes: None.
+    Methods: None.
+    """
     pass
+i
