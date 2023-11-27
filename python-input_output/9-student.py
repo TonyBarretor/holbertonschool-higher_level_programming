907e583808ce4646754129d11dee94a1ab8d5276
TonyBarretor <tony.barretor@gmail.com>
2023-11-26 20:57:55 -0800

    fix: structure in tak9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
+"""Module to define the Student class."""
 
 class Student:
-    """Defines a student with public instance attributes and a to_json"""
+    """Defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initializes a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        Returns:
-            dict: Dictionary representation of the Student instance.
-        """
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        """Retrieves a dictionary representation of a Student instance."""
+        json_dict = {}
+        for key, value in self.__dict__.items():
+            if not key.startswith("__"):
+                json_dict[key] = value
+        return json_dict
