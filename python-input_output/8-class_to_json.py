64807fa882962972b6d605bad7bb3013dbafb75f
TonyBarretor <tony.barretor@gmail.com>
2023-11-26 20:55:29 -0800

    fix:doc

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""Module to convert class instance to JSON representation."""
 
 def class_to_json(obj):
-    """
-    Returns the dictionary description with a simple data structure
-    for JSON serialization of an object.
-
-    Args:
-        obj: An instance of a Class.
-    """
-    json_dict = obj.__dict__.copy()
-    json_dict["__class__"] = obj.__class__.__name__
+    """Converts a class instance to a JSON-serializable dictionary."""
+    json_dict = {}
+    
+    # Add all attributes that are not methods or private
+    for key, value in obj.__dict__.items():
+        if not callable(value) and not key.startswith("__"):
+            json_dict[key] = value
+    
     return json_dict
