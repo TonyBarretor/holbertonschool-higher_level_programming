b86474b261c72f68f9080d6bbb065a36b8b5016e
TonyBarretor <tony.barretor@gmail.com>
2024-02-11 20:19:23 -0800

    feat: task 3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,17 @@
+// 3-script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleHeader = document.getElementById("toggle_header");
+    const header = document.querySelector("header");
+  
+    toggleHeader.addEventListener("click", function() {
+      if (header.classList.contains("red")) {
+        header.classList.remove("red");
+        header.classList.add("green");
+      } else {
+        header.classList.remove("green");
+        header.classList.add("red");
+      }
+    });
+  });
+  
