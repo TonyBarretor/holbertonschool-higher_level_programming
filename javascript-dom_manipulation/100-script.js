44f94e54d9623bbf6317c9986d150c059924be82
TonyBarretor <tony.barretor@gmail.com>
2024-02-11 20:35:57 -0800

    feat: task 9

---
+++ javascript-dom_manipulation/100-script.js
@@ -0,0 +1,26 @@
+// 100-script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addItem = document.getElementById("add_item");
+    const removeItem = document.getElementById("remove_item");
+    const clearList = document.getElementById("clear_list");
+    const myList = document.querySelector(".my_list");
+  
+    addItem.addEventListener("click", function() {
+      const newItem = document.createElement("li");
+      newItem.textContent = "Item";
+      myList.appendChild(newItem);
+    });
+  
+    removeItem.addEventListener("click", function() {
+      const lastItem = myList.lastElementChild;
+      if (lastItem) {
+        myList.removeChild(lastItem);
+      }
+    });
+  
+    clearList.addEventListener("click", function() {
+      myList.innerHTML = "";
+    });
+  });
+  
