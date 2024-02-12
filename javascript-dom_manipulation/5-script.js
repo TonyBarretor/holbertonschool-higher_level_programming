d1580829b53afed502ab6fa940746dbe73756a73
TonyBarretor <tony.barretor@gmail.com>
2024-02-11 20:25:09 -0800

    feat: task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const updateHeader = document.getElementById("update_header");
+    const header = document.querySelector("header");
+  
+    updateHeader.addEventListener("click", function() {
+      header.textContent = "New Header!!!";
+    });
+  });
+  
